fb9ab858d60c0adaeacae38f5ae3ff710d15fe92
lahmar wissem <91917987+wiswis5@users.noreply.github.com>
2022-05-06 23:50:40 +0100

    new commit

---
+++ src/main/java/com/wissem/produits/repos/CategorieRepository.java
@@ -0,0 +1,12 @@
+package com.wissem.produits.repos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wissem.produits.entities.Categorie;
+
+public interface CategorieRepository extends JpaRepository<Categorie, Long> {
+
+}
+
+
+
+
